e686fe302bc0bda24ac8ff74f1dce9f1f4cdbb8f
Gerard Juan <gerardandrejuan@gmail.com>
2023-05-08 03:38:27 -0400

    create app.js and add reset on form submit

---
+++ app.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	// code to submit form data here
+
+	// reset form fields
+	form.reset();
+	nameInput.focus();
+});
